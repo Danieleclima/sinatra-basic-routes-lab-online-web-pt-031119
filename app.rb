3cd41e6fc9a692b2756928a08c1a528b8360635f
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-06-13 18:38:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
+    "My name is Dani"
+  end
+  
+  get '/hometown' do
+    "My hometown is Madrid"
+  end
+  
+  get 'favorite-song' do
     
   end
+  
 end
